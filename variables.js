8e9608e36d5b4b8eb54b6212e8059a9e07a572e1
Dana Zack <danaezack@gmail.com>
2023-08-27 16:07:29 -0600

    Fixed quotation marks on reassigned string variables

--- variables.js
+++ variables.js
@@ -7,7 +7,7 @@ var age = 29
 var paidDeposit = true
 var paidTuition = true
 
-var firstName = John
-var lastName = Doe 
+var firstName = "John"
+var lastName = "Doe" 
 
 console.log(firstName, lastName, age, paidTuition)
